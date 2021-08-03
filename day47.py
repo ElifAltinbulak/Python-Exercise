280022a79c3594f668c2710d652cdc81ba3d58fe
ElifAltinbulak <76043798+ElifAltinbulak@users.noreply.github.com>
2021-08-03 22:57:09 +0300

    Add files via upload

---
+++ day47.py
@@ -0,0 +1,7 @@
+#day47
+#Kullanıcı tarafından girilen sayının rakamlarını çarpan program (sıfırları hariç tut)
+çarpma=1
+while True:
+    sayı=int(input("Bir sayı giriniz: "))
+    çarpma*=sayı
+    print(f"Sayının çarpımı: {çarpma}")
