620134e53fe7cb6c44d70cab6ad4aa13deef5821
ElifAltinbulak <76043798+ElifAltinbulak@users.noreply.github.com>
2021-08-03 22:32:54 +0300

    Add files via upload

---
+++ day26.py
@@ -0,0 +1,5 @@
+#day26
+#fonksiyon kullaranak genişliği ve yüksekliği girilen dikdörtgenin alanını hesaplayan python örneği
+def dikdörtgenalan(kısa,uzun):
+    return kısa*uzun
+print(dikdörtgenalan(5,4))
