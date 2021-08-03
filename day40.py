5adb39e48939eec52db23f5793e9d9799b827964
ElifAltinbulak <76043798+ElifAltinbulak@users.noreply.github.com>
2021-08-03 22:49:58 +0300

    Add files via upload

---
+++ day40.py
@@ -0,0 +1,8 @@
+#day40
+#aşağıdaki kodun çıktısını yazınınız pythoda tek karakterden oluşan değerleri tek tırnak içinde tanımlayabilirsiniz
+ders="KODLAMA"
+liste=[]
+for i in ders:
+    liste.append(i)
+print(liste)
+    
