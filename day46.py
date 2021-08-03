9ba0b9030c3b94804349f21594bb9e68c0ddeb66
ElifAltinbulak <76043798+ElifAltinbulak@users.noreply.github.com>
2021-08-03 22:55:50 +0300

    Add files via upload

---
+++ day46.py
@@ -0,0 +1,8 @@
+#day46
+#kullanıcının girdiği sayının rakamlarını toplayan python örneği
+toplam=0
+while True:
+    sayı=int(input("Bir sayı giriniz: "))
+    toplam+=sayı
+    print(f"Sayının toplamı: {toplam}")
+    
