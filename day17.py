e4742173e0ad21d77aabd22f88eeab00b4abb7a8
ElifAltinbulak <76043798+ElifAltinbulak@users.noreply.github.com>
2021-08-03 22:05:07 +0300

    Add files via upload

---
+++ day17.py
@@ -0,0 +1,7 @@
+#day17
+#kenarları girilen dikdörgenin alanı ve çevresi bulan python örneği
+kısa=int(input("kısa kenar:"))
+uzun=int(input("uzun kenar: "))
+alan=kısa*uzun
+cevre=(kısa*2)+(uzun*2)
+print(f"ALAN:{alan}\nÇEVRE:{cevre}")
