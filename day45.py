b297c65f462cbe47d40bac542029134914d20c4f
ElifAltinbulak <76043798+ElifAltinbulak@users.noreply.github.com>
2021-08-03 22:54:52 +0300

    Add files via upload

---
+++ day45.py
@@ -0,0 +1,10 @@
+#day45
+#ekran çıktısı aşağıdaki gibi olan kodu while döngüsü ile yazınız
+print("WHİLE")
+i=1
+while i<=12:
+    print(f"{i}.sınıf")
+    i+=1
+print("FOR")
+for i in range(1,13):
+    print(f"{i}.sınıf")
