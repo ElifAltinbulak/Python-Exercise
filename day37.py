d01060d7d9d92ca009046965b61023c9908df728
ElifAltinbulak <76043798+ElifAltinbulak@users.noreply.github.com>
2021-08-03 22:46:07 +0300

    Add files via upload

---
+++ day37.py
@@ -0,0 +1,7 @@
+#day37
+# en sevdiginiz 3 meyveyi liste haline getirerek ekrana yazdırınız
+liste=[]
+for i in range(1,4):
+    meyve=input(f"en sevdiğiniz {i} meyve:")
+    liste.append(meyve)
+print(liste)
