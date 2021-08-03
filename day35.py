5b1ec0b75fb5f382d93d523c227bce2fa224dcd7
ElifAltinbulak <76043798+ElifAltinbulak@users.noreply.github.com>
2021-08-03 22:41:14 +0300

    Add files via upload

---
+++ day35.py
@@ -0,0 +1,5 @@
+#day35
+#python fonksiyon kullanarak dikdörtgen alanı hesaplama
+def Alan_dikdörtgen(kısa,uzun):
+    return f"Kısa kenar:{kısa}\nUzun kenar:{uzun}\nAlan: {kısa*uzun}"
+print(Alan_dikdörtgen(6,3))
