cbcf296c7f1d3a3cc4b8d095a4ffc15302d425b9
ElifAltinbulak <76043798+ElifAltinbulak@users.noreply.github.com>
2021-08-03 22:52:32 +0300

    Add files via upload

---
+++ day43.py
@@ -0,0 +1,6 @@
+#day43
+#10-20 arası sayılardan oluşan sayılar isimli bir liste oluşturun ardından bu listede bulunan çift sayılar ekrana yazdırınız
+liste=[10,11,12,13,14,15,16,17,18,19,20]
+for i in liste:
+    if i%2==0:
+        print(i)
