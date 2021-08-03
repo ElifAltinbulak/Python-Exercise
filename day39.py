5751f5750236aad9b0dd4210c3e767073bae9edd
ElifAltinbulak <76043798+ElifAltinbulak@users.noreply.github.com>
2021-08-03 22:48:49 +0300

    Add files via upload

---
+++ day39.py
@@ -0,0 +1,6 @@
+#day39
+#haftanın günledinden pazartesi ile başlayan ve cuma ile biten bir liste oluşturunuz oluşturduğunuz listenin indeksi 4 olan elemanını ekrana yazdırınız
+liste=["Pazartesi","Salı","Çarşamba","Perşembe","Cuma"]
+print(liste[4])
+for i in liste:
+    print(i)
