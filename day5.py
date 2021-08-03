193205ac24cfb60f32422090261c38fbc650eb7b
ElifAltinbulak <76043798+ElifAltinbulak@users.noreply.github.com>
2021-08-03 21:58:59 +0300

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,6 @@
+#day5
+#girilen vize ve final notu ortalaması hesaplayan python örneği
+vize=int(input("Vize notu: "))
+final=int(input("Final notu: "))
+ortalama=(vize*0.3)+(final*0.7)
+print(f"Sonuç: {ortalama}")
