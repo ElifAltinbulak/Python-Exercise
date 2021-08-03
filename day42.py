d84349bd50c5097f508b9597fdcee67a65c102ef
ElifAltinbulak <76043798+ElifAltinbulak@users.noreply.github.com>
2021-08-03 22:51:47 +0300

    Add files via upload

---
+++ day42.py
@@ -0,0 +1,5 @@
+#day42
+#hafta_ıcı isimli bir liste oluşturarak haftanını günlerini ekliyenez daha sonra sıraysıyla cuma ve cumartesi günlerinin listede olup olmadığını kontrol ediniz
+hafta_ıcı=["Pazartesi","Salı","Çarşamba","Perşembe","Cuma"]
+print("Cuma" in hafta_ıcı)
+print("Cumartesi" in hafta_ıcı)
