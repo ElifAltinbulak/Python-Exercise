039f27996cc131d615daa50179d59db2076cce3b
ElifAltinbulak <76043798+ElifAltinbulak@users.noreply.github.com>
2021-08-03 22:00:00 +0300

    Add files via upload

---
+++ day8.py
@@ -0,0 +1,7 @@
+#day8
+#girilen sayının çift mi tek mi olduğunu bulunuz
+a=int(input("sayı giriniz: "))
+if a%2==0:
+    print(f"Sayınız {a} ve çift sayıdır")
+else:
+    print(f"Sayınız {a} ve tek sayıdır")
