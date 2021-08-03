5481fb84009f62269dfff3035fb0e2531d0a5b10
ElifAltinbulak <76043798+ElifAltinbulak@users.noreply.github.com>
2021-08-03 23:06:16 +0300

    Add files via upload

---
+++ day54.py
@@ -0,0 +1,10 @@
+#day54
+#mutlak değer hesaplamak için gerekli olan python kodu
+sayı=int(input("Bir sayı giriniz: "))
+if sayı<0:
+    sayı*=(-1)
+    print(sayı)
+else:
+    sayı*=1
+    print(sayı)
+    
