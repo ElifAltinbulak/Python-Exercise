5877ff72ff12ab9b05c2a7ec94cab58dcf6816d3
ElifAltinbulak <76043798+ElifAltinbulak@users.noreply.github.com>
2021-08-03 21:59:21 +0300

    Add files via upload

---
+++ day6.py
@@ -0,0 +1,7 @@
+#day6
+#girilen 3 yazılı notunun ortalamasını bulan python örneği
+y1=int(input("1.sınav notunuzu giriniz: "))
+y2=int(input("2.sınav notunuzu giriniz: "))
+y3=int(input("3.sınav notunuzu giriniz: "))
+ortalama=(y1+y2+y3)/3
+print(f"Sonuç:{ortalama}")
