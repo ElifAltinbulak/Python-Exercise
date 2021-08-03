51ee79761ef50e6b6331ed63dc32b978d0db1109
ElifAltinbulak <76043798+ElifAltinbulak@users.noreply.github.com>
2021-08-03 22:47:59 +0300

    Add files via upload

---
+++ day38.py
@@ -0,0 +1,3 @@
+#day38
+#sırarıyla pi sayısı inç birininin cm olarak karşılığı mikroişlemcilerin kısaltması kullandığınız işletim sisteminin adı ve 48 bitin byte olarak karşılığını bir liste halien getirerek ekrana yazdırınız
+print(list([3.14,2.54,"CPU","WINDOWS 10",6]))
